b7703c7eae105eca5e928d21d6f10ac3b7632b9e
SgoSkz <seangotskillz@gmail.com>
2022-01-10 10:37:58 -0600

    Removed things and switched to enums

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,10 @@ use std::{env, io::Error, io::ErrorKind};
 // I swear, this will hopefully, probably never change I think, maybe...
 const BASE: &str = "https://youtube.com/results?";
 
-// Coz no make client again and no compile regex again
-/// Created for the purpose of constants and not recompiling the regex. While this is a script that
-/// only runs once, the full purpose is for a TUI that utilizes the client more than once
-struct Client {
-    client: reqwest::Client,
-    re_pat: regex::Regex,
-    query: Query,
+enum QResults {
+    Video(Vec<Video>),
+    // Plylst,
+    None,
 }
 
 // Get results and store each individual one in a struct
@@ -29,51 +26,30 @@ struct Video {
 
 // Store all the results in here. Idk why I'm doing this, kinda just assuming it'll make something
 // easier in the future
-struct Query {
-    query: String,
-    json: String,
-    vid: Vec<Video>,
+// struct QueryResults {
+    // vid: Vec<Video>,
     // plylst: Vec<Playlist>,
-}
+// }
 
-#[tokio::main]
-async fn main() {
-    // Get command line arguments and store first one as query. I am well aware this breaks when
-    // there is no arguments provided, IDK how to solve this
-    let args: Vec<String> = env::args().collect();
-    let query: &str = &args[1];
-
-    // Create the thing that does the thing
-    let mut local: Client = Client {
-        client: reqwest::Client::new(),
-        // NOTE: Part 1, Important Probably
-        // This right here is prob the most important part of scraping the videos. The regex finds
-        // the section of the HTML that contains basically all of the page data as a json. See
-        // prep_json to see the next part of preparing the json
-        re_pat: regex::Regex::new(r"var ytInitialData =(.*?);</script>").unwrap(),
-        query: Query {
-            query: "".to_owned(),
-            json: "".to_owned(),
-            vid: Vec::new(),
-            // plylst: Vec::new(),
-        },
-    };
-
-    // Unimportant stuff, just jump to definition
-    let html = local.search(query.to_owned()).await.unwrap();
-    let data = local.get_json(html.to_owned()).await.unwrap();
-    let json = local.prep_json(data).await;
-    local.pop_videos(json).await;
-    local.list_vid().await;
+// Coz no make client again and no compile regex again
+/// Created for the purpose of constants and not recompiling the regex. While this is a script that
+/// only runs once, the full purpose is for a TUI that utilizes the client more than once
+struct Client {
+    client: reqwest::Client,
+    re_pat: regex::Regex,
+    // query: QueryResults,
 }
 
 impl Client {
+    // async fn search(query_results: QResults) -> QResults {
+    //     QResults { Video: }
+    // }
+
     /// Query Youtube and get HTML contents
     /// takes the self.query.query and searches it on youtube
     ///
     /// return: Result containing either HTML string or Error
-    async fn search(&mut self, query: String) -> Result<String, Error> {
-        self.query.query = query.to_owned();
+    async fn get_html(&mut self, query: String) -> Result<String, Error> {
         let res = self
             .client
             .get(format!("{}search_query={}", BASE, query))
@@ -121,49 +97,123 @@ impl Client {
                 .to_owned()
         ))
         .unwrap();
-        self.query.json = format!("{}", req_parse);
-        self.query.json.to_owned()
+        format!("{}", req_parse)
     }
 
-    /// Populate video vector
-    async fn pop_videos(&mut self, data: String) -> () {
-        let reparse: Vec<Value> = serde_json::from_str(&data).unwrap();
-        for data in reparse {
-            if data["videoRenderer"].is_null() {
-                continue;
-            }
-            self.query.vid.push(Video {
-                // NOTE: Part 3, Finding the video stuff
-                // This is the ID, AKA, the part after https://youtube.com/watch?v=[THIS PART]
-                vid_id: data["videoRenderer"]["videoId"]
-                    .as_str()
-                    .unwrap()
-                    .to_owned(),
-                // This is the title of the video
-                title: data["videoRenderer"]["title"]["runs"][0]["text"]
-                    .as_str()
-                    .unwrap()
-                    .to_owned(),
-                // This is the thumbnail link of the video
-                thumb: data["videoRenderer"]["thumbnail"]["thumbnails"][0]["url"]
-                    .as_str()
-                    .unwrap()
-                    .to_owned(),
-            });
+    async fn get_results(&self, json: String, query_results: QResults) -> QResults {
+        let reparse: Vec<Value> = serde_json::from_str(&json).unwrap();
+        match query_results {
+            QResults::Video(mut v) => { 
+                for data in reparse {
+                    if data["videoRenderer"].is_null() {
+                        continue;
+                    }
+                    v.push(Video {
+                        // NOTE: Part 3, Finding the video stuff
+                        // This is the ID, AKA, the part after https://youtube.com/watch?v=[THIS PART]
+                        vid_id: data["videoRenderer"]["videoId"]
+                            .as_str()
+                            .unwrap()
+                            .to_owned(),
+                        // This is the title of the video
+                        title: data["videoRenderer"]["title"]["runs"][0]["text"]
+                            .as_str()
+                            .unwrap()
+                            .to_owned(),
+                        // This is the thumbnail link of the video
+                        thumb: data["videoRenderer"]["thumbnail"]["thumbnails"][0]["url"]
+                            .as_str()
+                            .unwrap()
+                            .to_owned(),
+                    });
+                }
+                QResults::Video(v)
+            },
+            QResults::None => QResults::None,
         }
     }
+
+    // /// Populate video vector
+    // async fn pop_videos(&mut self, data: String) -> () {
+    //     let reparse: Vec<Value> = serde_json::from_str(&data).unwrap();
+    //     for data in reparse {
+    //         if data["videoRenderer"].is_null() {
+    //             continue;
+    //         }
+    //         self.query.vid.push(Video {
+    //             // NOTE: Part 3, Finding the video stuff
+    //             // This is the ID, AKA, the part after https://youtube.com/watch?v=[THIS PART]
+    //             vid_id: data["videoRenderer"]["videoId"]
+    //                 .as_str()
+    //                 .unwrap()
+    //                 .to_owned(),
+    //             // This is the title of the video
+    //             title: data["videoRenderer"]["title"]["runs"][0]["text"]
+    //                 .as_str()
+    //                 .unwrap()
+    //                 .to_owned(),
+    //             // This is the thumbnail link of the video
+    //             thumb: data["videoRenderer"]["thumbnail"]["thumbnails"][0]["url"]
+    //                 .as_str()
+    //                 .unwrap()
+    //                 .to_owned(),
+    //         });
+    //     }
+    // }
 
     /// Progressively getting lazier
     /// Lists the videos inside the video vector
-    async fn list_vid(self) -> () {
-        for data in self.query.vid {
-            println!(
-                r#"Video Title:	{}
-Video ID:	{}
-Video Thumb:	{}"#,
-                data.title, data.vid_id, data.thumb
-            );
+    async fn list_vid(&self, json: String) -> () {
+        match self.get_results(json, QResults::Video(Vec::new())).await {
+            QResults::Video(v) => {
+                for data in v {
+                    println!(r#"Video Title: {}
+Video ID:    {}
+Video Thumb: {}"#, data.title, data.vid_id, data.thumb);
+                }
+            },
+            QResults::None => println!("Nothing found...")
         }
+        // for data in self.query.vid {
+        //     println!(
+        //         r#"Video Title:	{}
+// Video ID:	{}
+// Video Thumb:	{}"#,
+        //         data.title, data.vid_id, data.thumb
+        //     );
+        // }
     }
-
 }
+
+#[tokio::main]
+async fn main() {
+    // Will use clap in the future
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        return ()
+    }
+    let query: &str = &args[1];
+
+    // Create the thing that does the thing
+    let mut local: Client = Client {
+        client: reqwest::Client::new(),
+        // NOTE: Part 1, Important Probably
+        // This right here is prob the most important part of scraping the videos. The regex finds
+        // the section of the HTML that contains basically all of the page data as a json. See
+        // prep_json to see the next part of preparing the json
+        re_pat: regex::Regex::new(r"var ytInitialData =(.*?);</script>").unwrap(),
+        // query: QueryResults {
+        //     // json: "".to_owned(),
+        //     vid: Vec::new(),
+        //     // plylst: Vec::new(),
+        // },
+    };
+
+    // Unimportant stuff, just jump to definition
+    let html = local.get_html(query.to_owned()).await.unwrap();
+    let data = local.get_json(html.to_owned()).await.unwrap();
+    let json = local.prep_json(data).await;
+    // local.pop_videos(json.to_owned()).await;
+    local.list_vid(json.to_owned()).await;
+}
+
